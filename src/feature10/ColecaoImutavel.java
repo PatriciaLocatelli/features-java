ed410d0b954749639e5dc2eae1337c065d3f52e7
patriciaLocatelli <paty.lw502@gmail.com>
2020-02-10 09:58:29 -0300

    Features Java 10

---
+++ src/feature10/ColecaoImutavel.java
@@ -0,0 +1,52 @@
+package feature10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class ColecaoImutavel {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Roger");
+        lista.add("Jéssica");
+        lista.add("Patricia");
+        lista.add("Thiago");
+        lista.add("Gabriel");
+
+        var copiaLista = Set.copyOf(lista);
+        copiaLista.forEach(System.out::println);
+
+        System.out.println("\nAdicionar a string 'Oi' em copiaLista \n");
+        try {
+            copiaLista.add("Oi");
+            copiaLista.forEach(System.out::println);
+        } catch (UnsupportedOperationException e) {
+            System.out.println("Deu erro :'( UnsupportedOperationException");
+        }
+
+        System.out.println("\nUsando o Set");
+        var collectionSet = Set.of("Cachorro", "Gato", "Papagaio", "Cavalo");
+        collectionSet.forEach(System.out::println);
+
+        System.out.println("\nVou adicionar a String: Morango");
+
+        try {
+            collectionSet.add("Morango");
+            collectionSet.forEach(System.out::println);
+        } catch (UnsupportedOperationException e) {
+            System.out.println("Deu erro :'( UnsupportedOperationException");
+        }
+
+        try {
+            var collectionMap = Map.of(1, "Casa", 2, "Carro", 3, "Piscina");
+            collectionMap.forEach((k, v) -> System.out.println("Chave : " + k + " " + " Valor: " + v));
+
+            System.out.println("\n Vou tentar adicionar a String Gato");
+            collectionMap.put(4, "Gato");
+        } catch (UnsupportedOperationException e) {
+            System.out.println("Deu erro :'( UnsupportedOperationException");
+        }
+
+    }
+}
---
+++ src/feature10/LocalVariableTypeInference.java
@@ -0,0 +1,19 @@
+package feature10;
+
+
+import java.util.Arrays;
+
+public class LocalVariableTypeInference {
+    public static void main(String[] args) {
+        var versao = getVersao();
+        System.out.println(versao);
+
+        var nome = Arrays.asList("Gabriel", "Roger", "Thiago", "Jéssica", "Patricia");
+        System.out.println(nome);
+
+    }
+
+    public static String getVersao() {
+        return "Estou usando a versão: Java 10";
+    }
+}
